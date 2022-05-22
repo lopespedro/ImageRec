bc420b13bafa5219afd1488f8be1fb86291263e9
Pedro Lopes <pedro.lopes.pordev@gmail.com>
2022-05-22 16:54:32 +0100

    add webConfig.js

---
+++ WebConfig.js
@@ -0,0 +1,6 @@
+class WebConfig {
+
+  static PORT = 8095;
+};
+
+export default WebConfig;
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 8095
+import WebConfig from './WebConfig.js';
+import express from 'express';
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Node.JS + Apache!')
-})
+const port = WebConfig.PORT;
 
 app.listen(port, () => {
-})
+
+  console.log(`server Running on Port: ${port}`);
+});
+
+app.get('/', (req, res) => {
+
+  res.sendFile('/views/index.html', { root: '.' });
+});
+
